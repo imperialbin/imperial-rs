317075021c4169a19ecbae0bac76705c0805475e
ripSquid <44409107+ripSquid@users.noreply.github.com>
2021-07-30 12:28:42 +0200

    Added the init/retrieve function on the whole imperialbinclient

--- src/imperialbin/mod.rs
+++ src/imperialbin/mod.rs
@@ -1,5 +1,6 @@
 
 pub mod post_request_builder;
+pub mod retrieve_request_builder;
 
 // Function to create an empty Imperialbin Client
 pub fn init() -> ImperialbinClient {
@@ -21,5 +22,9 @@ impl ImperialbinClient {
         // Creating a PostRequestBuilder which you can change different arguments with.
         post_request_builder::new().api_token(self.api_token.clone()).code(code)
 
+    }
+
+    pub fn retrieve(&self, document_id: String) -> retrieve_request_builder::RetrieveRequestBuilder {
+        retrieve_request_builder::new().document_id(document_id).api_token(self.api_token.clone())
     }
 }
